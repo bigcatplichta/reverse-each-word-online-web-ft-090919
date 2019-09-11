aab32579e15b3962e5cdd3e37bf5ec4c77b1bf10
Matt Plichta <bigcatplichta@gmail.com>
2019-09-11 19:22:10 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -12,7 +12,7 @@ require 'pry'
 def reverse_each_word(string)
   word_list = string.split
   reversed_words = []
-  word_list.each do | word |
+  word_list.collect do | word |
     reversed_words << word.reverse
   end
   reversed_words.join(" ")
